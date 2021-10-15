70969bbcbbdca495f47f1db609ea5ef00248f6a6
SajawalHassan <scientificninja1f12@gmail.com>
2021-10-15 17:55:10 +0500

    Add more flexibility for module

--- face_mesh_module.py
+++ face_mesh_module.py
@@ -17,19 +17,22 @@ class FaceMeshDetector():
         self.mpDraw = mp.solutions.drawing_utils
         self.drawSpec = self.mpDraw.DrawingSpec(thickness=1, circle_radius=1, color=(0, 255, 0))
     
-    def detectFaceMesh(self, img, draw=True):
+    def detectFaceMesh(self, img, draw=True, coordinates=False):
         # Convert img rgb for mediapipe
         imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
-                
         # Feed results to mediapipe
         results = self.faceMesh.process(imgRGB)
 
+        # Geting face coordinates
         faces = results.multi_face_landmarks
 
-        if draw:
-            if faces:
-                for face in faces:
+        if draw: # If draw is true
+            if faces: # And detects a face
+                for face in faces: # For each face detected
                     self.mpDraw.draw_landmarks(img, face, self.mpFaceMesh.FACEMESH_CONTOURS,
-                    self.drawSpec, self.drawSpec)
+                    self.drawSpec, self.drawSpec) # Draw face mesh on detected face
+
+        if coordinates: # If coordinates is true
+            print(faces) # Print face coordinates
 
